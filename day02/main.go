04be502b6207042aaa407dd69362ec65997c3fdd
Ville Koivisto <ville.koivisto@solita.fi>
2022-12-02 23:36:42 +0200

    Renaming...

--- day02/main.go
+++ day02/main.go
@@ -15,65 +15,63 @@ import (
 	"os"
 )
 
-var getWinner = map[byte]byte{
+var getWinningShape = map[byte]byte{
 	'A' : 'B',
 	'B' : 'C',
 	'C' : 'A',
 }
 
-var getLoser = map[byte]byte{
+var getLosingShape = map[byte]byte{
 	'A' : 'C',
 	'B' : 'A',
 	'C' : 'B',
 }
 
 func main() {
-	g, err := os.Open("game.txt")
+	f, err := os.Open("game.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer g.Close()
+	defer f.Close()
 
 	normalScores := []int{}
 	strategyScores := []int{}
-	
-	s := bufio.NewScanner(g)
+	s := bufio.NewScanner(f)
 	for s.Scan() {
 		game := s.Text()
-		o, p := game[0], game[2]
-		normalScores = append(normalScores, playNormal(o, p))
-		strategyScores = append(strategyScores, playStrategy(o, p))
+		opponent, me := game[0], game[2]
+		normalScores = append(normalScores, playNormal(opponent, me))
+		strategyScores = append(strategyScores, playStrategy(opponent, me))
 	}
-
+	
 	totalNormal, totalStrategy := 0, 0
-	for _, p := range normalScores {
-		totalNormal += p
+	for _, s := range normalScores {
+		totalNormal += s
 	}
-	for _, p := range strategyScores {
-		totalStrategy += p
+	for _, s := range strategyScores {
+		totalStrategy += s
 	}
-
 	fmt.Println(totalNormal, totalStrategy)
 }
 
-func playNormal(o byte, p byte) int {
-	op, me := shapeToValue(o), shapeToValue(p)
-	if op-me == -2 || op-me == 1 {
-		return me
-	} else if op-me == -1 || op-me == 2 {
-		return me + 6
-	} else { return me + 3 }
+func playNormal(o byte, m byte) int {
+	ov, mv := shapeToValue(o), shapeToValue(m)
+	if ov-mv == -2 || ov-mv == 1 {
+		return mv
+	} else if ov-mv == -1 || ov-mv == 2 {
+		return mv+6
+	} else { return mv+3 }
 }
 
-func playStrategy(o byte, p byte) int {	
-	if string(p) == "X" {
-		losingShape := getLoser[o]
+func playStrategy(o byte, m byte) int {	
+	if string(m) == "X" {
+		losingShape := getLosingShape[o]
 		return shapeToValue(losingShape)
-	} else if string(p) == "Y" {
-		return shapeToValue(o) + 3
+	} else if string(m) == "Y" {
+		return shapeToValue(o)+3
 	} else {
-		winningShape := getWinner[o]
-		return shapeToValue(winningShape) + 6
+		winningShape := getWinningShape[o]
+		return shapeToValue(winningShape)+6
 	}
 }
 
