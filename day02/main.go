07db1eb92611a584187af40cf667899396c12ebd
Ville Koivisto <ville.koivisto@solita.fi>
2022-12-02 23:20:43 +0200

    Day 2 / part 2

--- day02/main.go
+++ day02/main.go
@@ -4,7 +4,6 @@ Advent of Code: Day 2
 Elfs are playing rock paper scissors and you want to
 know the tournament outcome without (part 1) and
 with following a strategy guide (part 2).
-
 */
 
 package main
@@ -16,6 +15,18 @@ import (
 	"os"
 )
 
+var getWinner = map[byte]byte{
+	'A' : 'B',
+	'B' : 'C',
+	'C' : 'A',
+}
+
+var getLoser = map[byte]byte{
+	'A' : 'C',
+	'B' : 'A',
+	'C' : 'B',
+}
+
 func main() {
 	g, err := os.Open("game.txt")
 	if err != nil {
@@ -23,27 +34,30 @@ func main() {
 	}
 	defer g.Close()
 
+	normalScores := []int{}
+	strategyScores := []int{}
+	
 	s := bufio.NewScanner(g)
-	pointList := []int{}
 	for s.Scan() {
 		game := s.Text()
 		o, p := game[0], game[2]
-		pointList = append(pointList, playGame(o, p))
+		normalScores = append(normalScores, playNormal(o, p))
+		strategyScores = append(strategyScores, playStrategy(o, p))
 	}
 
-	total := 0
-	for _, p := range pointList {
-		total += p
+	totalNormal, totalStrategy := 0, 0
+	for _, p := range normalScores {
+		totalNormal += p
+	}
+	for _, p := range strategyScores {
+		totalStrategy += p
 	}
 
-	fmt.Println(total)
+	fmt.Println(totalNormal, totalStrategy)
 }
 
-func playGame(o byte, p byte) int {
-	op := shapeToValue(o)
-	me := shapeToValue(p)
-	
-	// lose, win, draw
+func playNormal(o byte, p byte) int {
+	op, me := shapeToValue(o), shapeToValue(p)
 	if op-me == -2 || op-me == 1 {
 		return me
 	} else if op-me == -1 || op-me == 2 {
@@ -51,8 +65,20 @@ func playGame(o byte, p byte) int {
 	} else { return me + 3 }
 }
 
-func shapeToValue(m byte) int {
-	switch value := string(m); value {
+func playStrategy(o byte, p byte) int {	
+	if string(p) == "X" {
+		losingShape := getLoser[o]
+		return shapeToValue(losingShape)
+	} else if string(p) == "Y" {
+		return shapeToValue(o) + 3
+	} else {
+		winningShape := getWinner[o]
+		return shapeToValue(winningShape) + 6
+	}
+}
+
+func shapeToValue(shape byte) int {
+	switch value := string(shape); value {
 	case "A", "X":
 		return 1
 	case "B", "Y":
