acb074bf9aad21a0f4b894b42fd690566268e6e0
Ville Koivisto <ville.koivisto@solita.fi>
2022-12-04 17:23:15 +0200

    Basic logic for part 1

--- day04/main.go
+++ day04/main.go
@@ -1,11 +1,16 @@
 /*
 Advent of Code: Day 4
 
+Check how many pairs overlap fully (part 1).
 
 */
 
 package main
 
 func main() {
-	// TODO: all
+	/* TODO:
+		Part 1
+		- read pairs and convert to slice of values,  maybe ints if fine 
+		- compare slices if one is a subset of another
+	*/
 }
