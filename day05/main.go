07b8c9ee64bf56aba6fe1dcb30f0d52c50eebe97
Ville Koivisto <ville.koivisto@solita.fi>
2022-12-05 16:20:40 +0200

    Type for crate stacks

--- day05/main.go
+++ day05/main.go
@@ -10,12 +10,36 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
 
+type allStacks struct {
+	st1 []byte
+	st2 []byte
+	st3 []byte
+	st4 []byte
+	st5 []byte
+	st6 []byte
+	st7 []byte
+	st8 []byte
+	st9 []byte
+}
+
+var stacks = allStacks{
+[]byte{'S','C','V','N'},
+[]byte{'Z','M','J','H','N','S'},
+[]byte{'M','C','T','G','J','N','D'},
+[]byte{'T','D','F','J','W','R','M'},
+[]byte{'P','F','H'},
+[]byte{'C','T','Z','H','J'},
+[]byte{'D','P','R','Q','F','S','L','Z'},
+[]byte{'C','S','L','H','D','F','P','W'},
+[]byte{'D','S','M','P','F','N','G','Z'},
+}
+
 func main() {
-	
 	m, err := os.Open("moves.txt")
 	if err != nil {
 		log.Fatalln(err)
@@ -24,6 +48,18 @@ func main() {
 
 	s := bufio.NewScanner(m)
 	for s.Scan() {
-		// TODO: all
+		rawMove := s.Text()
+		fmt.Println(rawMove)
 	}
+	fmt.Println(stacks)
 }
+
+/* TODO:
+- parsi syötteestä
+	- kuinka monta konttia siirretään
+	- lähde
+	- kohde
+- toteuta algoritmi
+	- firt in last out
+
+*/
