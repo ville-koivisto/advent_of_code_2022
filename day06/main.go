857ab7c512d0c3c03caf6c6fab15eb6a206378dc
Ville Koivisto <ville.koivisto@solita.fi>
2022-12-06 19:37:03 +0200

    Part 2

--- day06/main.go
+++ day06/main.go
@@ -3,6 +3,8 @@ Advent of Code: Day 6
 
 Find combination of four different characters, the
 start-of-packet marker, from the datastream (part 1).
+Also, tune in your device to find start of the
+message marker of 14 chars long (part 2).
 */
 
 package main
@@ -25,18 +27,23 @@ func main() {
 	for s.Scan() {
 		data := s.Text()
 		for i := range data {
-			if isUnique(data, i) {
-				fmt.Printf("Start marker end found at %vth character", i+4)
-				fmt.Println()
+			if isMarker(data, i, 3) {
+				fmt.Printf("Start marker end (packet) found at char: %v\n", i+4)
+				break
+			}
+		}
+		for i := range data {
+			if isMarker(data, i, 13) {
+				fmt.Printf("Start marker end (message) found at char: %v\n", i+14)
 				os.Exit(0)
 			}
 		}
 	}
 }
 
-func isUnique(data string, index int) bool {
+func isMarker(data string, index int, marker int) bool {
 	checkUnique := make(map[byte]bool,0)
-	for i := index; i <= index+3; i++ {
+	for i := index; i <= index+marker; i++ {
 		if checkUnique[data[i]] {
 			return false
 		}
